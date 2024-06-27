7d7c5b575b5a14f417394b5ea081506c4041b4e8
melychst <melychst@users.noreply.github.com>
2024-06-27 08:58:53 +0300

    Columns

--- wp-content/themes/deepdive/template-parts/blocks/block-duble-columns-revers.php
+++ wp-content/themes/deepdive/template-parts/blocks/block-duble-columns-revers.php
@@ -4,8 +4,15 @@
         $left_column = get_field('left_column');
         if( !empty($left_column) ):
             $bg_type = $left_column['background_type'];
+            $borders = $left_column['borders'];
+            
+            $border_top = $borders['border_top'];
+            $border_bottom = $borders['border_bottom'];
+            $border_right = $borders['border_right'];
+        
+            $border_css = "border-width: ". $border_top ."px ". $border_right ."px ". $border_bottom ."px 0px";
     ?>
-            <div class="column column-left" style="background-<?php echo $bg_type; ?>: <?php echo $bg_type == 'image' ? "url(". $left_column['background_image']['url'] .")" : $left_column['background_color'] ?>;">
+            <div class="column column-left" style="background-<?php echo $bg_type; ?>: <?php echo $bg_type == 'image' ? "url(". $left_column['background_image']['url'] .")" : $left_column['background_color'] ?>; <?php echo $border_css;?>;">
                 <div class="column-image-wrap">
                     <?php if( $left_column['image'] ): ?>
                         <img src="<?php echo $left_column['image']['url']; ?>" alt="<?php echo $left_column['image']['alt']; ?>">
@@ -19,8 +26,15 @@
     <?php
         $right_column = get_field('right_column');
         if( !empty($right_column) ):
+            $borders = $right_column['borders'];
+        
+            $border_top = $borders['border_top'];
+            $border_bottom = $borders['border_bottom'];
+            $border_left = $borders['border_left'];
+        
+            $border_css = "border-width: ". $border_top ."px 0px ". $border_bottom ."px ". $border_left ."px";
     ?>
-            <div class="column column-right" style="background-color: <?php echo $right_column['background_color']; ?>">
+            <div class="column column-right" style="background-color: <?php echo $right_column['background_color']; ?>; <?php echo $border_css;?>;">
                 <div class="content-wrap">
                     <h3><?php echo $right_column['title']; ?></h3>
                     <div class="content"><?php echo $right_column['description']; ?>
--- wp-content/themes/deepdive/template-parts/blocks/block-duble-columns.php
+++ wp-content/themes/deepdive/template-parts/blocks/block-duble-columns.php
@@ -2,8 +2,16 @@
     <?php
         $left_column = get_field('left_column');
         if( !empty($left_column) ):
+        $borders = $left_column['borders'];
+        
+        $border_top = $borders['border_top'];
+        $border_bottom = $borders['border_bottom'];
+        $border_right = $borders['border_right'];
+       
+        $border_css = "border-width: ". $border_top ."px ". $border_right ."px ". $border_bottom ."px 0px";
+        
     ?>
-            <div class="column column-left column-content" style="background-color:<?php echo $left_column['background_color']; ?>">
+            <div class="column column-left column-content" style="background-color:<?php echo $left_column['background_color']; ?>; <?php echo $border_css; ?>;">
                 <div class="content-wrap">
                     <h3><?php echo $left_column['title']; ?></h3>
                     <div class="content"><?php echo $left_column['description']; ?></div>
@@ -20,8 +28,16 @@
             $media_type = $right_column['media_type'];
             $media_image = $right_column['image'];
             $media_animation = $right_column['animation_url'];
+
+            $borders = $right_column['borders'];
+            
+            $border_top = $borders['border_top'];
+            $border_bottom = $borders['border_bottom'];
+            $border_left = $borders['border_left'];
+        
+            $border_css = "border-width: ". $border_top ."px 0px ". $border_bottom ."px ". $border_left ."px";
     ?>
-            <div class="column column-right column-image" style="background-<?php echo $bg_type; ?>: <?php echo $bg_type == 'image' ? "url(". $right_column['background_image']['url'] .")" : $right_column['background_color'] ?>;">
+            <div class="column column-right column-image" style="background-<?php echo $bg_type; ?>: <?php echo $bg_type == 'image' ? "url(". $right_column['background_image']['url'] .")" : $right_column['background_color'] ?>; <?php echo $border_css; ?>;">
                 <div class="column-image-wrap">
                     
                     <?php if( ($media_type == 'animation') && !empty($media_animation) ): ?>
