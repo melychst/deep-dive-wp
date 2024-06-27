c5f9639bf69a02db28fa9f4c37c70ad3bde12eb5
melychst <melychst@users.noreply.github.com>
2024-06-27 15:14:09 +0300

    404

--- wp-content/themes/deepdive/404.php
+++ wp-content/themes/deepdive/404.php
@@ -1,3 +1,21 @@
 <?php get_header(); ?>
+    <div id="primary" class="content-area">
 
+        <section class="page-404">
+            <div class="container">
+                <div class="content-wrap">
+                    <?php
+                        $animation = get_field('animation_404', 'option');
+                        if( $animation != '' ):
+                    ?>
+                            <dotlottie-player src="<?php echo $animation; ?>" background="transparent" speed="1"  loop autoplay></dotlottie-player>
+                    <?php
+                        endif;
+                    ?>
+                    <h3><?php echo get_field('title_404', 'option'); ?></h3>
+                </div>
+            </div>
+        </section>
+
+    </div><!-- .content-area -->
 <?php get_footer(); ?>
